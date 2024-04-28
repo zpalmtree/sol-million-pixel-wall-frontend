cb6c4340dc2a98301cd220fd9c8f8ad6bd8d67ac
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2024-04-28 18:15:47 -0400

    Initial commit

---
+++ src/app/wall/page.tsx
@@ -0,0 +1,9 @@
+export default function PixelWall() {
+    return (
+        <div className='flex items-center justify-center h-screen'>
+            <span className='text-xl'>
+                Wall
+            </span>
+        </div>
+    );
+}
