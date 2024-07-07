1821779b9abf2a98c0b3b79e2c91012789334a4c
zpalmtree <22151537+zpalmtree@users.noreply.github.com>
2024-07-07 18:06:47 -0400

    Update default prices

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,9 +1,7 @@
 import { LAMPORTS_PER_SOL } from '@solana/web3.js';
 
-//export const PRICE_PER_BRICK = 0.25 * LAMPORTS_PER_SOL;
-
-export const PRICE_PER_BRICK = 0.001 * LAMPORTS_PER_SOL;
-export const PRICE_PER_BRICK_EDIT = 0.001 * LAMPORTS_PER_SOL;
+export const PRICE_PER_BRICK = 0.25 * LAMPORTS_PER_SOL;
+export const PRICE_PER_BRICK_EDIT = 0.0069 * LAMPORTS_PER_SOL;
 
 export const CANVAS_WIDTH = 600;
 export const CANVAS_HEIGHT = 600;
